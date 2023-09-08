398de725870e7b171de124183843b5d3a867eadc
Sebastian Olsson <sebol117@student.liu.se>
2023-09-08 17:36:42 +0200

    Added dependency installation for Linux Mint 21 (Vanessa) to InstallPackagesUbuntu.sh

--- InstallPackagesUbuntu.sh
+++ InstallPackagesUbuntu.sh
@@ -3,7 +3,16 @@
 sudo apt-get update
 
 readonly ubuntu_release="$(grep "DISTRIB_RELEASE" /etc/lsb-release | sed -e 's/DISTRIB_RELEASE=//')"
-if [[ "${ubuntu_release}" == "22.04" ]]; then
+if [[ "${ubuntu_release}" == "22.04" ]]; then 
+  sudo apt-get install -qq \
+    g++ cmake \
+    libeigen3-dev freeglut3-dev libopencv-dev \
+    qtdeclarative5-dev libqt5multimedia5 \
+    protobuf-compiler libprotobuf-dev \
+    libdc1394-25 libdc1394-dev \
+    libv4l-0
+elif [[ "${ubuntu_release}" == "21" ]]; then
+  # Linux Mint Vanessa
   sudo apt-get install -qq \
     g++ cmake \
     libeigen3-dev freeglut3-dev libopencv-dev \
